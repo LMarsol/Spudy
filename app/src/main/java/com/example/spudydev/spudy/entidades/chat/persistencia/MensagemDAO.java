636ba261e582f9739745afe1f96a7c70df1e11bd
Lucas Marsol <Lucasmarsolv@hotmail.com>
2018-07-17 13:28:13 -0300

    Spudy

---
+++ app/src/main/java/com/example/spudydev/spudy/entidades/chat/persistencia/MensagemDAO.java
@@ -0,0 +1,41 @@
+package com.example.spudydev.spudy.entidades.chat.persistencia;
+
+import com.example.spudydev.spudy.entidades.chat.dominio.Mensagem;
+import com.example.spudydev.spudy.infraestrutura.persistencia.AcessoFirebase;
+import com.google.firebase.database.DatabaseException;
+
+
+/**
+ * The type Mensagem dao.
+ */
+public final class MensagemDAO {
+
+    private MensagemDAO(){
+        //Construtor vazio
+    }
+
+    /**
+     * Enviar mensagem boolean.
+     *
+     * @param texto         the texto
+     * @param codigoTurma the codigo da turma
+     * @return the boolean
+     */
+//Enviar mensagem para a turma
+    public static boolean enviarMensagem(String texto, String codigoTurma){
+        boolean verificador;
+        try {
+            Mensagem mensagem = new Mensagem();
+
+            mensagem.setAutor(AcessoFirebase.getUidUsuario());
+            mensagem.setTexto(texto);
+            AcessoFirebase.getFirebase().child("chat").child(codigoTurma).push().setValue(mensagem);
+            verificador = true;
+
+        } catch (DatabaseException e){
+            verificador = false;
+        }
+        return verificador;
+    }
+}
+
